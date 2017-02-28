c744b73772ebd5a9b8c338fd701b3bccaa573bbc
Ori <hello.ori@gmail.com>
2017-02-28 12:08:36 +0200

    Clean sheet to start the 2nd example - math game

--- learning/hello-world/src/App.js
+++ learning/hello-world/src/App.js
@@ -1,63 +1,17 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
-import $ from 'jquery'; 
-
-/*class Card extends Component {
-  getInitialState (){
-    return {};
-  };
-  componentDidMount(){
-   
-    var component = this;
-    $.get("https://api.github.com/users/petehunt", function (data) {
-      document.getElementById('logger').innerHTML  = "ajax";
-      component.setState(data);
-    })
-  };
-
-  render() {
-    return (
-      <div>
-        <img src={this.state.avatar_url} width="80" />
-        <h3>{this.state.name}</h3>
-        <hr />
-      </div>
-    ) 
-  }
-}*/
+import $ from 'jquery';
 
 
-var Card = React.createClass({
-  
-  getInitialState: function (){
-    return {};
-  },
-  componentDidMount: function (){
-   
-    var component = this;
-    $.get("https://api.github.com/users/petehunt", function (data) {
-      console.log("in $.get");
-      document.getElementById('logger').innerHTML  = "ajax";
-      component.setState(data);
-    })
-  },
-  render: function () {
-    return (
-      <div>
-        <img src={this.state.avatar_url} width="80" alt="Card photo"/>
-        <h3>{this.state.name}</h3>
-        <hr/>
-      </div>
-      )
-  }
-})
 
 class App extends Component {
+  constructor(props, context) {
+    super(props, context);
+  };
   render() {
     return (
       <div>
-        <Card login="petehunt" />
+        
       </div>
     );
   }
