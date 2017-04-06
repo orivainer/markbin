ace28aec66f68304d3b6290fed1ccac08ba4ef69
Ori <hello.ori@gmail.com>
2017-04-06 12:40:57 +0300

    Added bins collection + content till lecture 84

---
+++ client/components/app.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './header';
+
+class App extends React.Component {
+    constructor(props, context) {
+        super(props, context);
+    }
+
+    render() {
+        return (
+            <Header/>
+        );
+    }
+}
+export default App;
